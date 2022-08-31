40b929778e7a489a2a6414a062b26d29b9a428e4
Adibatla Datta Pranav <71169207+adpranav@users.noreply.github.com>
2022-08-31 21:50:21 +0530

    Create delete_node_recursion.java

---
+++ delete_node_recursion.java
@@ -0,0 +1,34 @@
+Given a singly linked list of integers and position 'i', delete the node present at the 'i-th' position in the linked list recursively
+
+/*
+
+    Following is the Node class already written for the Linked List
+
+    class LinkedListNode<T> {
+        T data;
+        LinkedListNode<T> next;
+    
+        public LinkedListNode(T data) {
+            this.data = data;
+        }
+    }
+
+*/
+
+public class Solution {
+
+
+	public static LinkedListNode<Integer> deleteNodeRec(LinkedListNode<Integer> head, int pos) {
+    	//Your code goes here
+        if(head==null){
+            return head;
+        }
+        if(pos==0){
+            head=head.next;
+            return head;
+        }
+        LinkedListNode<Integer> newNode = deleteNodeRec(head.next,pos-1);
+        head.next=newNode;
+        return head;
+    }
+}
